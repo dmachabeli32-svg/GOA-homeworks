08d7827090767e6a8b03f5db734c5441c805546c
dmachabeli32-svg <dmachabeli32@gmail.com>
2025-10-02 20:22:37 +0400

    second commit

---
+++ Level 01/homework/hm1.py
@@ -0,0 +1,32 @@
+#1) print() ფუნქციის (რამოდენიმეჯერ) გამოყენებით, დაპრინტეთ სამკუთხედი, ნებისმიერი სიმბოლოთი
+print("     *    ")
+print("    ***    ")
+print("   *****    ")
+print("  *******    ")
+
+#2) გამოიტანეთ თქვენი საყვარელი 5 საჭმელი, ყველა თითო ხაზზე
+food="khinkali"
+print(food)
+food1="shawrma"
+print(food1)
+food2="khachapuri"
+print(food2)
+food3="lobiani"
+print(food3)
+
+food4="kubdari"
+print(food4)
+#3) მარტივი გამოიტანეთ Hello და თქვენი სახელი
+print("Hello dachi")
+ 
+#4) დაპტინტეთ ოთკუთხედი და ოთკუთხედის შუაში დაწერეთ რაიმე ტექსტი
+print("###########")
+print("## Hello ##")
+print("###########")
+
+#5) გააკეთეთ მარტივი მენიუ, ყველა საჭმელს მიუწერეთ ფასი (ყველა ახალ ხაზებზე გამოიტანეთ)
+print("khinkali-1Gel")
+print("shawrma-12Gel")
+print("khachapuri-18Gel")
+print("lobiani-16Gel")
+print("mtsvadi-10Gel")
